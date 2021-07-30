978d54dabeec1320f22a8784902d172a3b8b2a2a
ihateuu <replacement506@gmail.com>
2021-07-30 18:41:22 +0300

    Initial Commit

---
+++ 08 Map Hackwich/_8_Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_Map_HackwichApp.swift
+//  08 Map Hackwich
+//
+//  Created by Kareem Almomen on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct _8_Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
